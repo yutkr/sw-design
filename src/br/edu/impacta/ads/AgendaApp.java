1128c67ed53f0d0afffc9264dbb3017bc8e68895
1600771 <1600771@sala30917.fit.net>
2017-03-07 22:09:33 -0300

    Versão Amadora

---
+++ src/br/edu/impacta/ads/AgendaApp.java
@@ -0,0 +1,109 @@
+package br.edu.impacta.ads;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import static java.lang.System.out;
+
+public class AgendaApp {
+	private static Scanner entrada = new Scanner(System.in);
+	private static List<Contato> contatos = new ArrayList<>();
+	public static void main (String[] args) {
+		boolean sair = false;
+		while (!sair) {
+			int opcao = apresentarMenuPrincipal();
+			switch(opcao) {
+			case 1: inserirContato(); break;
+			case 2: buscarContato(); break;
+			case 3: sair = true; break;
+			default: out.println ("ERRO: opção inválida!");
+			}
+		}
+		out.println("\nFim do Programa");
+	}
+	
+	private static int apresentarMenuPrincipal(){
+		boolean inteiro = false;
+		int opcao = 0;
+		while (!inteiro) {
+			out.println("\nAGENDA TELEFÔNICA");
+			out.println("(1) Inserir");
+			out.println("(2) Buscar");
+			out.println("(3) Sair");
+			out.println("Escolha uma opção: ");
+			String s = entrada.nextLine();
+			try {
+				opcao = Integer.parseInt(s);
+				inteiro = true;
+			} catch(Exception e) {
+				out.println("ERRO: opção deve ser um valor inteiro!");
+			}
+		}
+		return opcao;
+	}
+	private static void inserirContato() {
+		out.println("\nINSERÇÃO DE NOVO CONTATO:");
+		String nome = lerNome();
+		String telefone = lerTelefone();
+		Contato c = new Contato(nome, telefone);
+		if (contatos.contains(c)) {
+			out.println("Este contato já existe");
+		}
+		else {
+			contatos.add(c);
+			out.println("Contato inserido");
+		}
+	}
+	
+	private static String lerNome() {
+		boolean valido = false;
+		String nome = "";
+		while (!valido) {
+			out.print("Nome: ");
+			nome = entrada.nextLine();
+			if (nome.length()==0 || nome.length()>200) {
+				out.println("ERRO: nome de tamanho inválido");
+			} else {
+				valido = true;
+			}
+		}
+		return nome;
+	}
+	
+	private static String lerTelefone() {
+		boolean valido = false;
+		String telefone = "";
+		while (!valido) {
+			out.println("Telefone: ");
+			telefone = entrada.nextLine();
+			if (telefone.length()==0 || telefone.length()>25) {
+				out.println("ERRO: telefone de tamanho inválido");
+			} else {
+				valido = true;
+			}
+		}
+		return telefone;
+	}
+	
+	private static void buscarContato() {
+		out.println ("\nBUSCA DE CONTATOS:");
+		String nome = lerNome();
+		List<Contato> resultado = new ArrayList<>();
+		for (Contato c: contatos) {
+			if (nome.equals(c.getNome())) {
+				resultado.add(c);
+			}
+		}
+		if (resultado.size() == 0) {
+			out.println("Não há contato com este nome");
+		} else {
+			out.println("\nResultado da busca: ");
+			for (Contato c: resultado) {
+				out.println(c);
+			}
+		}
+		
+				
+	}
+
+}
---
+++ src/br/edu/impacta/ads/Contato.java
@@ -0,0 +1,60 @@
+package br.edu.impacta.ads;
+
+public class Contato {
+	private String nome;
+	private String telefone;
+	public Contato() {
+		super();
+	}
+	public Contato(String nome, String telefone) {
+		super();
+		this.nome = nome;
+		this.telefone = telefone;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	@Override
+	public String toString() {
+		return "Contato [nome=" + nome + ", telefone=" + telefone + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((telefone == null) ? 0 : telefone.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Contato other = (Contato) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (telefone == null) {
+			if (other.telefone != null)
+				return false;
+		} else if (!telefone.equals(other.telefone))
+			return false;
+		return true;
+	}
+
+}
